eb9eff5fa93e4f523601f8c953c48fa2619c534d
akashc <akashc@qburst.com>
2020-11-19 16:22:10 +0530

    Added validation for xray images.

--- ML/utils.py
+++ ML/utils.py
@@ -11,3 +11,14 @@ def predict_covid(image, covid_model_path='./best_covid_classifier'):
     categories = [classes[j] for index, j in enumerate(predictions)]
     probablities = [predicted_y[index][j] for index, j in enumerate(predictions)]
     return (predictions, categories, probablities)
+
+
+def predict_xray(image, model_path='./best_xray_identifier'):
+    classes = ['No-X', 'X']
+    loaded_model = load_model(model_path)
+    # loaded_model.summary()
+    predicted_y = loaded_model.predict(tf.expand_dims(image, -1)) * 100
+    predictions = [1 if j[0] > .5 else 0 for index, j in enumerate(predicted_y)]
+    categories = [classes[j] for index, j in enumerate(predictions)]
+    probablities = [j[0] for index, j in enumerate(predicted_y)]
+    return (predictions, categories, probablities)
--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
 
-from flask import Flask, render_template, request, redirect, url_for  # These are all we need for our purposes
+from flask import Flask, render_template, request, redirect, url_for, \
+    make_response  # These are all we need for our purposes
 from flask_cors import CORS
 
 import tensorflow as tf
 from keras.preprocessing.image import load_img, img_to_array
 from werkzeug.utils import secure_filename
 
-from ML.utils import predict_covid
+from ML.utils import predict_covid, predict_xray
 from urls import urls
 from log import Logger
 
@@ -17,7 +18,8 @@ CORS(app)
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['DEBUG'] = False
+
+urls['error_text'] = ''
 
 
 def allowed_file(filename):
@@ -32,7 +34,14 @@ def index():
 
 @app.route("/x-ray-test")
 def form():
-    return render_template("form.html", **urls)
+    trial = request.args.get('again')
+    if trial == '1':
+        urls['error_text'] = 'Try again with a valid image.'
+    else:
+        urls['error_text'] = ''
+    response = make_response(render_template("form.html", **urls))
+    response.headers.add('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
+    return response
 
 
 @app.route("/test-result", methods=['POST'])
@@ -41,21 +50,26 @@ def predict():
         image_file = request.files.get('x-ray', None)
         if image_file and allowed_file(image_file.filename):
             filename = secure_filename(image_file.filename)
-            if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                os.mkdir(app.config['UPLOAD_FOLDER'])
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             image_file.save(file_path)
             img = img_to_array(load_img(file_path, target_size=(200, 200))) / 255.0
             os.remove(file_path)
             img = tf.expand_dims(img, axis=0)
-            # TODO : Check whether input image is X-ray or Not
-            output = predict_covid(img, covid_model_path='./ML/best_covid_classifier')
-            accuracy = output[2][0] * 100
-            category = output[1][0]
-            args = {'diagnosis': category, 'accuracy': str(accuracy) + '%'}
-            for i in urls.items():
-                args[i[0]] = i[1]
-            return render_template("result.html", **args)
+            img_type = predict_xray(img, model_path='./ML/best_xray_identifier')
+            # Check classification accuracy for xray_identifier
+            if img_type[2][0] * 100 > 95 and img_type[0][0] == 1:
+                output = predict_covid(img, covid_model_path='./ML/best_covid_classifier')
+                accuracy = output[2][0] * 100
+                category = output[1][0]
+                args = {'diagnosis': category, 'accuracy': str(accuracy) + '%'}
+                for i in urls.items():
+                    args[i[0]] = i[1]
+                response = make_response(render_template("result.html", **args))
+                response.headers.add('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
+                return response
+            else:
+                return redirect(url_for('form', **{'again': '1'}))
+
         else:
             return redirect(url_for('error_500'))
 
